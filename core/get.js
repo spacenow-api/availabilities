997b45215cfeaa9ffe5f32bafd59d4dca1c40435
Lucas <csalucasnascimento@gmail.com>
2019-05-31 14:57:17 +1000

    availability by listing id

--- core/get.js
+++ core/get.js
@@ -6,8 +6,7 @@ export const main = async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     Key: {
-      listingId: event.listingId,
-      availabilityId: event.pathParameters.id
+      listingId: event.pathParameters.id
     }
   }
 
