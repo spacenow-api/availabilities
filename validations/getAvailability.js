0298e49934c96982f823d6d40d956e444192fbc5
Lucas <csalucasnascimento@gmail.com>
2019-06-03 16:07:47 +1000

    fix: availability by listing id returning 2 arrays

--- validations/getAvailability.js
+++ validations/getAvailability.js
@@ -4,7 +4,7 @@ const availability = async items => {
   let exceptionDates = Array();
 
   items.map((item) => {
-    item.bookingId ? bookingDates.concat(item.blockedDates) : exceptionDates.concat(item.blockedDates)
+    item.bookingId ? bookingDates.conca(...bookingDates, item.blockedDates) : exceptionDates.concat(...exceptionDates, item.blockedDates)
   })
 
   return ({ bookingDates, exceptionDates });
