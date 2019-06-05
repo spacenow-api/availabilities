03a10b3f6d282ad05d41ff1fe81c759e781c26e3
Lucas <csalucasnascimento@gmail.com>
2019-06-05 14:39:03 +1000

    feat: adding delete api

--- core/deleteByListing.js
+++ core/deleteByListing.js
@@ -3,6 +3,8 @@ import { success, failure } from "../libs/response-lib"
 
 export const main = async (event, context) => {
 
+  console.log("LISTING ID", event.pathParameters.id)
+
   const params = {
     TableName: process.env.tableName,
     Key: {
