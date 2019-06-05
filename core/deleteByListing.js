2a90ff1d06873c9a4c5744990444154769df2dad
Lucas <csalucasnascimento@gmail.com>
2019-06-05 15:00:28 +1000

    fix: delete id to string

--- core/deleteByListing.js
+++ core/deleteByListing.js
@@ -8,7 +8,7 @@ export const main = async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     Key: {
-      listingId: event.pathParameters.id
+      listingId: event.pathParameters.id.toString()
     }
   }
 
