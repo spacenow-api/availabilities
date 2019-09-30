1967d581398e757d938ee4aefba0924262177446
Arthemus <arthemus.moreira@gmail.com>
2019-09-30 12:50:30 +1000

    Getting availabilities by listing on mysql.

--- core/getByListing.js
+++ core/getByListing.js
@@ -1,24 +1,29 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
-import getAvailability from '../validations/getAvailability';
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import getAvailability from '../validations/getAvailability'
+import { Availabilities } from './../models'
 
-export const main = async (event, context) => {
-  const params = {
-    TableName: process.env.tableName,
-    FilterExpression: '#lId = :listingId',
-    ExpressionAttributeNames: {
-      '#lId': 'listingId'
-    },
-    ExpressionAttributeValues: {
-      ':listingId': event.pathParameters.id
-    }
-  };
+export const main = async (event) => {
   try {
-    const res = await dynamoDbLib.call('scan', params);
-    const availability = await getAvailability(res.Items);
-    return success({ availability });
+    // const res = await dynamoDbLib.call('scan', {
+    //   TableName: process.env.tableName,
+    //   FilterExpression: '#lId = :listingId',
+    //   ExpressionAttributeNames: {
+    //     '#lId': 'listingId'
+    //   },
+    //   ExpressionAttributeValues: {
+    //     ':listingId': event.pathParameters.id
+    //   }
+    // })
+    // const availability = await getAvailability(res.Items)
+    // return success({ availability })
+    const result = await Availabilities.findAll({
+      where: { listingId: event.pathParameters.id }
+    })
+    const availability = await getAvailability(result)
+    return success({ availability })
   } catch (err) {
-    console.error(err);
-    return failure({ status: false, error: err });
+    console.error(err)
+    return failure({ status: false, error: err })
   }
-};
+}
--- validations/getAvailability.js
+++ validations/getAvailability.js
@@ -1,17 +1,17 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
-const availability = async items => {
-  let bookingDates = Array();
-  let exceptionDates = Array();
-  items.map(item => {
+const availability = async (items) => {
+  let bookingDates = Array()
+  let exceptionDates = Array()
+  items.map((item) => {
     item.bookingId
       ? bookingDates.push(item.blockedDates)
-      : exceptionDates.push(item.blockedDates);
-  });
+      : exceptionDates.push(item.blockedDates)
+  })
   return {
     bookingDates: _.flattenDeep(bookingDates),
     exceptionDates: _.flattenDeep(exceptionDates)
-  };
-};
+  }
+}
 
-export default availability;
+export default availability
