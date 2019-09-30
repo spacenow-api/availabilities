480561707b46265baa7eede34d16cf94530b5a24
Arthemus <arthemus.moreira@gmail.com>
2019-09-30 12:40:57 +1000

    Deleting by listing on mysql.

--- core/deleteByListing.js
+++ core/deleteByListing.js
@@ -1,19 +1,20 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import { Availabilities } from './../models'
 
-export const main = async (event, context) => {
-  const params = {
-    TableName: process.env.tableName,
-    Key: {
-      listingId: event.pathParameters.id.toString()
-    }
-  };
-  
+export const main = async (event) => {
   try {
-    await dynamoDbLib.call('delete', params);
-    return success({ status: true });
+    // await dynamoDbLib.call('delete', {
+    //   TableName: process.env.tableName,
+    //   Key: {
+    //     listingId: event.pathParameters.id.toString()
+    //   }
+    // })
+    // return success({ status: true })
+    await Availabilities.destroy({ where: { listingId: event.pathParameters.id.toString() } })
+    return success({ status: true })
   } catch (err) {
-    console.error(err);
-    return failure({ status: false });
+    console.error(err)
+    return failure({ status: false })
   }
-};
+}
