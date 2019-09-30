6713ccbec1f5e044f499bc7ab2212a95b0048097
Arthemus <arthemus.moreira@gmail.com>
2019-09-30 12:38:34 +1000

    Deleting by booking on mysql.

--- core/deleteByBooking.js
+++ core/deleteByBooking.js
@@ -1,36 +1,39 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import { Availabilities } from './../models'
 
-export const main = async event => {
+export const main = async (event) => {
   if (event.pathParameters.id) {
     console.info(`Delete Availabilities by Booking ${event.pathParameters.id}`)
     try {
-      const scanResponse = await dynamoDbLib.call('scan', {
-        TableName: process.env.tableName,
-        FilterExpression: 'bookingId = :bookingId',
-        ExpressionAttributeValues: {
-          ':bookingId': event.pathParameters.id
-        }
-      });
-      if (scanResponse.Items.length > 0) {
-        const record = scanResponse.Items[0];
-        await dynamoDbLib.call('delete', {
-          TableName: process.env.tableName,
-          Key: {
-            listingId: record.listingId,
-            availabilityId: record.availabilityId
-          }
-        });
-        return success({ status: true });
-      } else {
-        console.warn(`Any availability found for the booking ${event.pathParameters.id}`);
-        return success({ status: true });
-      }
+      // const scanResponse = await dynamoDbLib.call('scan', {
+      //   TableName: process.env.tableName,
+      //   FilterExpression: 'bookingId = :bookingId',
+      //   ExpressionAttributeValues: {
+      //     ':bookingId': event.pathParameters.id
+      //   }
+      // });
+      // if (scanResponse.Items.length > 0) {
+      //   const record = scanResponse.Items[0];
+      //   await dynamoDbLib.call('delete', {
+      //     TableName: process.env.tableName,
+      //     Key: {
+      //       listingId: record.listingId,
+      //       availabilityId: record.availabilityId
+      //     }
+      //   });
+      //   return success({ status: true });
+      // } else {
+      //   console.warn(`Any availability found for the booking ${event.pathParameters.id}`);
+      //   return success({ status: true });
+      // }
+      await Availabilities.destroy({ where: { bookingId: event.pathParameters.id } })
+      return success({ status: true });
     } catch (err) {
-      console.error(err);
-      return failure({ status: false, error: err });
+      console.error(err)
+      return failure({ status: false, error: err })
     }
   } else {
-    return failure({ status: false, error: 'The booking ID is required.' });
+    return failure({ status: false, error: 'The booking ID is required.' })
   }
-};
+}
