f52b5df6065252cf3dc27448511f317a4fd55a54
Arthemus <arthemus.moreira@gmail.com>
2019-09-30 12:44:47 +1000

    Getting availability by id.

--- core/get.js
+++ core/get.js
@@ -1,22 +1,26 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import { Availabilities } from './../models'
 
-export const main = async (event, context) => {
-  const params = {
-    TableName: process.env.tableName,
-    FilterExpression: '#aId = :availabilityId',
-    ExpressionAttributeNames: {
-      '#aId': 'availabilityId'
-    },
-    ExpressionAttributeValues: {
-      ':availabilityId': event.pathParameters.id
-    }
-  };
+export const main = async (event) => {
   try {
-    const result = await dynamoDbLib.call('scan', params);
-    return success({ count: result.Items.length, bookings: result.Items });
+    // const result = await dynamoDbLib.call('scan', {
+    //   TableName: process.env.tableName,
+    //   FilterExpression: '#aId = :availabilityId',
+    //   ExpressionAttributeNames: {
+    //     '#aId': 'availabilityId'
+    //   },
+    //   ExpressionAttributeValues: {
+    //     ':availabilityId': event.pathParameters.id
+    //   }
+    // })
+    // return success({ count: result.Items.length, bookings: result.Items })
+    const availabilities = await Availabilities.findAll({
+      where: { availabilityId: event.pathParameters.id }
+    })
+    return success({ count: availabilities.length, bookings: availabilities })
   } catch (err) {
-    console.error(err);
-    return failure({ status: false, error: err });
+    console.error(err)
+    return failure({ status: false, error: err })
   }
-};
+}
